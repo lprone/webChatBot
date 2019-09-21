c3272df637d0e88b1bc879dd4bd68056c9b8747b
Lucas Prone <sac2087@gmail.com>
2019-09-21 03:16:37 -0300

    changed way to connect to api

--- chat.js
+++ chat.js
@@ -11,24 +11,14 @@ function send() {
     var chat = document.getElementById("chat");
 
     chat.value = addMessageFromUser(chat.value, message.value);
+    const url = 'https://lpronechatbotapi.herokuapp.com/answer?key=' + message.value;
+
     message.value = "";
 
-    const url = 'https://lpronerestapi.herokuapp.com';
-
-    axios.get(url)
-        .then(function (response) {
-            // handle success
-            chat.value = addMessageFromBot(chat.value, JSON.stringify(response.data, null, 2));
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
-        .finally(function () {
-            // always executed
-        });
-
-
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false);
+    xmlHttp.send();
+    chat.value = addMessageFromBot(chat.value, xmlHttp.response);
 }
 
 
